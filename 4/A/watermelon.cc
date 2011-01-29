c1320e1cc8c9be79e707a186fc98153b13fa9aea
Alexander Urieles <aeurielesn@gmail.com>
2011-01-28 22:05:46 -0500

    Add 4A - Watermelon in C++

---
+++ 4/A/watermelon.cc
@@ -0,0 +1,16 @@
+/*
+* STATUS = ACCEPTED
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int w;
+    scanf("%d",&w);
+    if(w%2||w<3) printf("NO\n");
+    else printf("YES\n");
+}
